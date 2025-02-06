2f607dc108106fccc2ef588e7a997159d5707112
DebugDynamo <er.ritikpal@gmail.com>
2025-02-06 15:58:27 +0530

    analytics added:

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,15 @@
-/* eslint-disable no-undef */
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from "path"
+  /* eslint-disable no-undef */
+  import { defineConfig } from 'vite'
+  import react from '@vitejs/plugin-react'
+  import path from "path"
 
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+  // https://vitejs.dev/config/
+  export default defineConfig({
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
-})
+  })
